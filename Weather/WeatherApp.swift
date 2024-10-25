616c18dcf89d6cbd8a9607504b6178f214815054
koulikmaity <maitykoulik2003@gmail.com>
2024-10-25 22:08:14 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Koulik Maity on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
